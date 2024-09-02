6332a4092ee0d477fd0dbccba3cfa3105e6eebc5
Scott Connor <scottconnor22@gmail.com>
2024-09-02 06:47:21 +0100

    commit all

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 
-
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astrotutorial001.netlify.app/",
+});
